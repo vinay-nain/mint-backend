1c645d68b5e2a7c74311777d58f9bfd899cfb657
Vinay Nain <vinaynain024@gmail.com>
2026-03-14 02:30:28 +0530

    Update config to use env variables

--- src/cloudinary.js
+++ src/cloudinary.js
@@ -1,15 +1,10 @@
 import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 
-// cloudinary.config({
-//     cloud_name: process.env.CLOUD_NAME,
-//     api_key: process.env.CLOUD_API_KEY,
-//     api_secret: process.env.CLOUD_API_SECRET,
-// });
 cloudinary.config({
-    cloud_name:"dnqfpy14q",
-    api_key: "418788342743685",
-    api_secret:"NsiNX7HDO76AM28O4ajfqKqSOss",
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
